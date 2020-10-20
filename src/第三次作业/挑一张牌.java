1147f6055026cd0d2bcf9dc6f58e3997b49e42bb
chorayya <473217413@qq.com>
2020-10-20 16:31:44 +0800

    3.24

---
+++ src/第三次作业/挑一张牌.java
@@ -0,0 +1,31 @@
+package 第三次作业;
+
+public class 挑一张牌 {
+	public static void main(String[] args) {
+		System.out.print("The card you picked is ");
+		int n1=(int)(Math.random()*13+1);
+		switch(n1) {
+		case 1:System.out.print("Ace ");break;
+		case 2:System.out.print("2 ");break;
+		case 3:System.out.print("3 ");break;
+		case 4:System.out.print("4 ");break;
+		case 5:System.out.print("5 ");break;
+		case 6:System.out.print("6 ");break;
+		case 7:System.out.print("7 ");break;
+		case 8:System.out.print("8 ");break;
+		case 9:System.out.print("9 ");break;
+		case 10:System.out.print("10 ");break;
+		case 11:System.out.print("Jcak ");break;
+		case 12:System.out.print("Queen ");break;
+		case 13:System.out.print("King ");break;
+		}
+		System.out.print("of ");
+		int n2=(int)(Math.random()*4+1);
+		switch(n2) {
+		case 1:System.out.print("Clubs");break;
+		case 2:System.out.print("Diamonds");break;
+		case 3:System.out.print("Hearts");break;
+		case 4:System.out.print("Spades");break;
+		}
+	}
+}
