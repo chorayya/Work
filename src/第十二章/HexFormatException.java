92247c20a9a8ad384a9c133f951f8c3e78063282
chorayya <473217413@qq.com>
2021-01-03 23:26:03 +0800

    12-8

---
+++ src/第十二章/HexFormatException.java
@@ -0,0 +1,56 @@
+package µÚÊ®¶þÕÂ;
+import java.util.Scanner;
+public class HexFormatException {
+
+	
+		public static void main(String[] args) {
+			Scanner input=new Scanner(System.in);
+			try {
+			System.out.print("Enter a hex number:");
+			String hex = input.next();
+			System.out.println("The decimal value for hex number "
+					+ hex + " is " + hexToDecimal(hex));
+			}
+			catch(HexFormatExceptionc ex) {
+				System.out.println(ex.toString() + " not a hex string.");
+			}
+
+		}
+		public static int hexToDecimal (String hex) throws HexFormatExceptionc {
+			int decimalValue=0;
+			for (int i = 0; i < hex.length(); i++) {
+				char hexChar = hex.charAt(i);
+				int judge=hexCharToDecimal(hexChar);
+				if(judge==-1) {
+					throw new HexFormatExceptionc(hex);
+				}
+				else
+					decimalValue += judge*Math.pow(16, (hex.length()-i-1));
+				
+			}
+			return decimalValue;
+		}
+		
+		public static int hexCharToDecimal (char ch) {
+			if (ch >='A' && ch <= 'F')
+				return 10+ch-'A';
+			else if(ch >='0' && ch <='9')
+				return ch-'0';
+			else
+				return -1;
+		}
+
+	}
+
+class HexFormatExceptionc extends Exception {
+	private String hex;
+	public HexFormatExceptionc(String hex) {
+		this.hex = hex;
+	}
+
+	@Override
+	public String getMessage() {
+		return this.hex;
+	}
+
+}
